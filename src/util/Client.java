488f47ce4ba672a983917da90c74a7a0fcb04d08
lazaro-dev <lazaro.sobral@facape.br>
2020-10-29 00:07:29 -0300

    calculadora

---
+++ src/util/Client.java
@@ -0,0 +1,93 @@
+package util;
+
+// Java implementation for a client
+// Save file as Client.java
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+// Client class
+public class Client
+{
+    public static void main(String[] args) throws IOException
+    {
+        try
+        {
+            Scanner scn = new Scanner(System.in);
+
+            InetAddress ip = InetAddress.getByName("localhost");
+            String str;
+            String str1;
+            String str3;
+            String str5;
+           
+            Socket s = new Socket(ip, 5057);
+            Socket s2 = new Socket(ip, 5057);
+
+            while (true)
+            {
+            	InputStream i = s.getInputStream();
+            	OutputStream o = s.getOutputStream();
+            	InputStream i2 = s2.getInputStream();
+            	OutputStream o2 = s2.getOutputStream();
+	        	 byte[] line1 = new byte[100];
+	        	 byte[] line2 = new byte[100];
+	        	 byte[] line3 = new byte[100];
+	        	 byte[] total = new byte[100];
+            	System.out.println("Digite o primeiro número:");
+                System.in.read(line1);              
+                System.out.println("Digite a operação: + => (soma) - => (subtração) * => (multiplicação) / => (divisão) r => raiz ^ => pontencia s => sair?");
+                System.in.read(line2);              
+                System.out.println("Digite o segundo número:");
+                System.in.read(line3);              
+
+                str1 = new String(line1);                
+                str = new String(line2);                
+                str3 = new String(line3);   
+                
+                String tot;
+//              System.out.println("entrei");
+//            	break;
+            	
+                if(str.trim().equals("+") || str.trim().equals("-") || str.trim().equals("*") || str.trim().equals("/")) {
+                	
+                	tot = str1.trim()+"#"+str.trim()+"#"+str3.trim();                    
+                	total = tot.getBytes();
+                	o.write(total);
+                	i.read(total);
+                }else if(str.trim().equals("r") || str.trim().equals("^") || str.trim().equals("%")){
+                	
+                	tot = str1.trim()+"#"+str.trim()+"#"+str3.trim();
+                	total = tot.getBytes();
+                	o2.write(total);
+                	i2.read(total);
+//                	System.out.println(tot.trim());
+                }
+                
+                
+				str5 = new String(total);
+				System.out.println(str5.trim());
+                
+                if(str.trim().equals("s"))
+                {
+                    System.out.println("Closing this connection : " + s);
+                    s.close();
+                    System.out.println("Connection closed");
+                    break;
+                }
+
+                //printing date or time as requested by client
+				//String received = dis.readUTF();
+				//System.out.println(received);
+            }
+
+            // closing resources
+//            scn.close();
+//            dis.close();
+//            dos.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/util/ServerB.java
@@ -0,0 +1,80 @@
+package util;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.net.*;
+
+public class ServerB
+{
+    public static void main(String[] args) throws IOException
+    {
+
+    	 try {
+             ServerSocket s = new ServerSocket(5057);
+             String str;
+             while (true) {
+                 Socket c = s.accept();
+                 InputStream i = c.getInputStream();
+                 OutputStream o = c.getOutputStream();
+                 do {
+                     byte[] line = new byte[100];
+                     i.read(line);
+                     str = new String(line);
+                 
+                     String total = calcular(str);
+                     o.write(total.getBytes());
+//                     System.out.println(str.trim());
+//                     str = new String(line);
+                     		
+                 } while ( !str.trim().equals("s") );
+                 c.close();
+             }
+         }
+         catch (Exception err){
+             System.err.println(err);
+         }
+    }
+    
+    public static String calcular(String str) {
+    	
+  		str.replace(" ", "").trim();
+  		char [] expre = str.toCharArray();
+  		String str1[] = new String[4];
+  		int h=0;
+  		for (int j=0; j < expre.length; j++) {
+  			
+  			if(Character.isDigit(expre[j])|| (expre[j] == '%') || (expre[j] == '^') || (expre[j] == 'r')) {
+  				
+  				if(Character.isDigit(expre[j])) {
+  						if(h>0 && Character.isDigit(expre[j-1])) {
+  							str1[h-1] = str1[h-1] + Character.toString(expre[j]);    
+  							h--;
+  						}else {
+  							str1[h] = Character.toString(expre[j]);  
+  						}
+  						    					
+  				}else {
+  					str1[h] = Character.toString(expre[j]);
+  				}
+  				h++;
+  			}
+  			
+  		}
+  		
+  		switch (str1[1]) {
+  		case "%":  			
+  			return "" + (Double.parseDouble(str1[0]) * (Double.parseDouble(str1[2])/100.00) );
+  			
+  		case "^":
+  			return "" + Math.pow(Double.parseDouble(str1[0]), Double.parseDouble(str1[2]));
+  			
+  		case "r":  			
+  			return "" + Math.pow(Double.parseDouble(str1[0]), (1/Double.parseDouble(str1[2])) );
+
+  		default:
+  			return "" ;					
+  		}
+  	    	
+      }	    	
+}
